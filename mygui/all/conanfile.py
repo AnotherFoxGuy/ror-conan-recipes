e0f89654df3da7b71a4a1715c57ebca0da4f45e0
Edgar <Edgar@AnotherFoxGuy.com>
2021-08-04 18:53:29 +0000

    :tada: Added mygui

---
+++ mygui/all/conanfile.py
@@ -0,0 +1,42 @@
+from conans import ConanFile, CMake, tools
+from conans.tools import os_info, SystemPackageTool
+
+
+class MyGUIConan(ConanFile):
+    name = "mygui"
+    license = "MIT"
+    url = "https://github.com/AnotherFoxGuy/conan-MyGUI"
+    description = "Fast, flexible and simple GUI."
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "cmake_paths"
+    exports_sources = "patches/**"
+
+    def requirements(self):
+        for req in self.conan_data["requirements"]:
+            self.requires(req)
+
+    def source(self):
+        tools.get(**self.conan_data["sources"][self.version], strip_root=True)
+        for patch in self.conan_data["patches"][self.version]:
+            tools.patch(**patch)
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.definitions['MYGUI_BUILD_DEMOS'] = 'OFF'
+        cmake.definitions['MYGUI_BUILD_DOCS'] = 'OFF'
+        cmake.definitions['MYGUI_BUILD_TEST_APP'] = 'OFF'
+        cmake.definitions['MYGUI_BUILD_PLUGINS'] = 'OFF'
+        cmake.definitions['MYGUI_BUILD_TOOLS'] = 'OFF'
+        cmake.definitions['MYGUI_RENDERSYSTEM'] = '3'
+        cmake.configure()
+        cmake.build()
+
+    def package(self):
+        cmake = CMake(self)
+        cmake.install()
+
+    def package_info(self):
+        self.cpp_info.includedirs = ['include/MYGUI']
+        # Directories where libraries can be found
+        self.cpp_info.libdirs = ['lib', 'lib/release', 'lib/debug']
+        self.cpp_info.libs = tools.collect_libs(self)
