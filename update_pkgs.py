5a12f3d839716207098fc4ec1f1a30d53bf67f42
Edgar <Edgar@AnotherFoxGuy.com>
2023-09-25 15:08:46 +0200

    :wrench: Added tool to automatically update packages

---
+++ update_pkgs.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+import os
+from glob import glob
+import yaml
+from packaging import version
+from lastversion import latest
+from urllib.request import urlretrieve
+from filehash import FileHash
+
+
+def update_pkg(repo, folder):
+    path = os.path.join(folder, "conandata.yml")
+    with open(path, "r") as file:
+        conandata = yaml.load(file, Loader=yaml.FullLoader)
+    v = sorted(conandata["sources"].items(), reverse=True)[0][0]
+    latest_version = latest(repo=repo, output_format="dict")
+    if latest_version["version"] > version.parse(v):
+        print(f'{repo} has newer version: {latest_version["version"]} (from: {v})')
+        url = f'https://github.com/{repo}/archive/refs/tags/{latest_version["tag_name"]}.tar.gz'
+        tmp_path, headers = urlretrieve(url)
+        hs = FileHash("sha256")
+        hash = hs.hash_file(tmp_path)
+        conandata["sources"][str(latest_version["version"])] = {
+            "url": url,
+            "sha256": hash,
+        }
+        print(f'Added new data: "{url}" "{hash}"')
+        with open(path, "w") as ofile:
+            yaml.dump(conandata, ofile, default_flow_style=False)
+    else:
+        print(f"{repo} has no update available")
+
+
+dirs = glob("./*/")
+for p in dirs:
+    path = os.path.join(p, "repoinfo.yml")
+    if not os.path.isfile(path):
+        continue
+    with open(path) as file:
+        repoinfo = yaml.load(file, Loader=yaml.FullLoader)
+        for i in repoinfo:
+            update_pkg(i["repo"], os.path.join(p, i["folder"]))
