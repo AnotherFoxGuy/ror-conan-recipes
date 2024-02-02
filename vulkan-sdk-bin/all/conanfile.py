b1a3d8662af10335e8aded5f6b7a40b750212ed7
Edgar <Edgar@AnotherFoxGuy.com>
2024-02-02 11:26:31 +0100

    :arrow_up: Updated VulkanSDK

--- vulkan-sdk-bin/all/conanfile.py
+++ vulkan-sdk-bin/all/conanfile.py
@@ -1,28 +1,36 @@
-from conans import ConanFile, tools
-from conans.tools import os_info
+from conan import ConanFile
+from conan.tools.files import get, collect_libs, download, copy
+import os
 
 
 class VulkanConan(ConanFile):
     name = "vulkan-sdk-bin"
     author = "Edgar Edgar@AnotherFoxGuy.com"
     settings = "os", "arch"
-    no_copy_source = True
 
     def requirements(self):
-      if os_info.is_windows:
-         self.requires("7zip/19.00")
+        if self.settings.os == "Windows":
+            self.requires("7zip/23.01")
 
-    def source(self):
-        if os_info.is_windows:
-            tools.download(**self.conan_data["sources-windows"][self.version], filename="vulkan-sdk.exe")
-            self.run("7z x vulkan-sdk.exe -ox86_64")
-        elif os_info.is_linux:
-            tools.get(**self.conan_data["sources-linux"][self.version], strip_root=True)
+    def build(self):
+        if self.settings.os == "Windows":
+            download(
+                self,
+                **self.conan_data["sources-windows"][self.version],
+                filename="vulkan-sdk.7z"
+            )
+            self.run("7z x vulkan-sdk.7z -ox86_64")
+        elif self.settings.os == "Linux":
+            get(self, **self.conan_data["sources-linux"][self.version], strip_root=True)
         else:
             raise Exception("Binary does not exist for these settings")
 
     def package(self):
-        self.copy("*", src="x86_64", dst=".")
+        copy(self, "*", os.path.join(self.source_folder, "x86_64"), self.package_folder)
 
     def package_info(self):
-        self.cpp_info.libs = self.collect_libs()
+        self.cpp_info.set_property("cmake_module_file_name", "Vulkan")
+        self.cpp_info.set_property("cmake_module_target_name", "Vulkan::Vulkan")
+        self.cpp_info.set_property("cmake_file_name", "Vulkan")
+        self.cpp_info.set_property("cmake_target_name", "Vulkan::Vulkan")
+        self.cpp_info.libs = collect_libs(self)
