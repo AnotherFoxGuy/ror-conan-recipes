5014cd7896fd262c2ba4047297b7192a55d58842
Edgar <Edgar@AnotherFoxGuy.com>
2024-05-14 14:30:03 +0200

    :bug: Fixed finding rapidjson in discord-rpc

--- discord-rpc/all/conanfile.py
+++ discord-rpc/all/conanfile.py
@@ -13,7 +13,7 @@ class DiscordrpcConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
 
     def requirements(self):
-        self.requires("rapidjson/cci.20220822") #find_package(RapidJSON REQUIRED)
+        self.requires("rapidjson/cci.20230929")
 
     def layout(self):
         cmake_layout(self)
@@ -27,20 +27,29 @@ class DiscordrpcConan(ConanFile):
         tc.generate()
 
     def _patch_sources(self):
-        replace_in_file(self,
+        replace_in_file(
+            self,
             os.path.join(self.source_folder, "CMakeLists.txt"),
             "find_file(RAPIDJSON NAMES rapidjson rapidjson-1.1.0 PATHS ${CMAKE_CURRENT_SOURCE_DIR}/thirdparty CMAKE_FIND_ROOT_PATH_BOTH)",
-            "find_package(RapidJSON REQUIRED)",
+            "find_path(RapidJSON_INCLUDE_DIRS NAMES rapidjson/rapidjson.h PATHS ${RAPIDJSON_INCLUDEDIR})",
         )
-        replace_in_file(self,
+        replace_in_file(
+            self,
             os.path.join(self.source_folder, "src", "CMakeLists.txt"),
             "${RAPIDJSON}/include",
             "${RapidJSON_INCLUDE_DIRS}",
         )
-        replace_in_file(self,
+        replace_in_file(
+            self,
             os.path.join(self.source_folder, "CMakeLists.txt"),
             "add_library(rapidjson STATIC IMPORTED ${RAPIDJSON})",
-            "# add_library(rapidjson STATIC IMPORTED ${RAPIDJSON})",
+            'message("Found rapidjson at ${RapidJSON_INCLUDE_DIRS}")',
+        )
+        replace_in_file(
+            self,
+            os.path.join(self.source_folder, "CMakeLists.txt"),
+            "CLANG_FORMAT_CMD",
+            "0"
         )
 
     def build(self):
